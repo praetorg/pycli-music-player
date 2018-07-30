fdc2fc881e7b2c1a04c5629fee1763430abbc57b
Unknown <praetorg@tutanota.com>
2018-07-30 02:36:17 -0400

    Added console for playback options

--- music.py
+++ music.py
@@ -5,6 +5,7 @@ import sys
 import shutil
 import random
 import subprocess
+import threading
 import musicformat
 
 
@@ -18,44 +19,136 @@ Short arguments may be combined, such as '-rs'.
 Ctrl-c to exit.
 """
 
+HELPER2 = """
+pycli-music console commands:
+    's', 'skip', 'n', 'next': Next song
+    'p', 'prev', 'b', 'back': Previous song
+    'x', 'exit', 'q', 'quit': Exit player
+    'repeat'                : Toggle repeat
+    'shuffle'               : Toggle shuffle
+"""
 
-def main(filename=None, shuffle=False, repeat=False):
-    if filename:
-        music = filename
-    else:
-        music = os.path.join(Path.home(), 'Music')
-    songs = list()
-    if os.path.isdir(music):
-        for root, dirs, files in os.walk(music):
-            for name in files:
-                songs.append(os.path.join(root, name))
-    else:
-        if os.path.isabs(music):
-            songs.append(music)
+
+MUSICPROCESS = None
+
+
+def main():
+    while True:
+        song = start.current()
+        play(song)
+        start.next()
+
+
+class Songs:
+    def __init__(self, filename=None, shuffle=False, repeat=False):
+        self.songs = list()
+        self.counter = 0
+        self.repeat = repeat
+        self.filename = filename
+        self.shuffle = shuffle
+        if filename:
+            music = filename
         else:
-            songs.append(os.path.join(os.getcwd(), music))
-    if shuffle:
-        random.shuffle(songs)
-    for song in songs:
-        try:
-            form = musicformat.music_format(song)
-            if form is None:
-                print(f'Discarding file, not a known music format: {song}')
-                songs.pop(song)
-                continue
-            print(f'Playing song: {song}')      
-            play(song)
-        except FileNotFoundError:
-            print('File not found, skipping.')
-    if repeat and len(songs) > 0:
-        main(filename, shuffle, repeat)
-    else:
-        print('Done, no more files.')
+            music = os.path.join(Path.home(), 'Music')
+        if os.path.isdir(music):
+            for root, dirs, files in os.walk(music):
+                for name in files:
+                    self.songs.append(os.path.join(root, name))
+        else:
+            if os.path.isabs(music):
+                self.songs.append(music)
+            else:
+                self.songs.append(os.path.join(os.getcwd(), music))
+        for song in self.songs:
+            try:
+                form = musicformat.music_format(song)
+                if form is None:
+                    self.songs.pop(song)
+            except FileNotFoundError:
+                self.songs.pop(song)
+        if len(self.songs) < 1:
+            print('\r\nNo valid files to play.')
+            shutdownfn()
+        if shuffle:
+            self.shufflefn()
 
+
+    def next(self):
+        if self.counter < len(self.songs) - 1:
+            self.counter+=1
+        elif self.repeat:
+            self.__init__(self.filename, self.shuffle, self.repeat)
+        else:
+            shutdownfn()
+
+
+    def shufflefn(self):
+        random.shuffle(self.songs)
+
+
+    def previous(self):
+        if self.counter > 0:
+            self.counter-=2
+
+
+    def current(self):
+        return self.songs[self.counter]
+
+
+    def repeat_toggle(self):
+        self.repeat = not self.repeat
+        if self.repeat:
+            printout(f'Repeat on.')
+        else:
+            printout(f'Repeat off.')
+
+    
+    def shuffle_toggle(self):
+        self.shuffle = not self.shuffle
+        if self.shuffle:
+            printout(f'Shuffle on.')
+            self.shufflefn()
+        else:
+            printout(f'Shuffle off.')
+
+
+    def end(self):
+        self.repeat = False
+        self.counter = len(self.songs) - 1
+        
 
 def play(song):
+    global MUSICPROCESS
+    printout(f'Playing song: {song}')
     command = f'{PLAYER} -nodisp -autoexit -hide_banner "{song}"'
-    subprocess.call(command, shell=True, bufsize=1, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, universal_newlines=True)
+    MUSICPROCESS = subprocess.Popen(command, shell=True, bufsize=1, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, universal_newlines=True)
+    MUSICPROCESS.wait()
+
+
+def console():
+    try:
+        while True:
+            control = None
+            control = input()
+            if control == 'skip' or control == 'next' or control == 's' or control == 'n':
+                MUSICPROCESS.terminate()
+            elif control == 'exit' or control == 'quit' or control == 'x' or control == 'q':
+                shutdownfn()
+            elif control == 'back' or control == 'prev' or control == 'p' or control =='b':
+                start.previous()
+                MUSICPROCESS.terminate()
+            elif control == 'help' or control == 'h' or control == '?':
+                printout(HELPER2)
+            elif control == 'repeat':
+                start.repeat_toggle()
+            elif control == 'shuffle':
+                start.shuffle_toggle()
+    except KeyboardInterrupt:
+        shutdownfn()
+
+
+def printout(statement):
+    print(f'\r\n{statement}\r\npycli-music>>> ', end='')
 
 
 def get_player():
@@ -64,8 +157,14 @@ def get_player():
     elif shutil.which("ffplay"):
         return "ffplay"
     else:
-        print("ffmpeg or avconf not found, exiting.")
-        exit()
+        printout("ffmpeg or avconf not found, exiting.")
+        shutdownfn()
+
+
+def shutdownfn():
+    MUSICPROCESS.terminate()
+    start.end()
+    sys.exit()
 
 
 PLAYER = get_player()
@@ -77,8 +176,8 @@ if __name__ == '__main__':
     filename = None
     if len(sys.argv) > 1:
         if "--help" in sys.argv or "--?" in sys.argv:
-            print(HELPER)
-            exit()
+            printout(HELPER)
+            shutdownfn()
         for arg in sys.argv[1:]:
             if arg.startswith('-'):
                 if '--' in arg:
@@ -93,7 +192,9 @@ if __name__ == '__main__':
             else:
                 filename = arg
     try:
-        main(filename, shuffle, repeat)
+        thread = threading.Thread(target=console)
+        thread.start()
+        start = Songs(filename, shuffle, repeat)
+        main()
     except KeyboardInterrupt:
-        print('\r\nExiting.')
-        exit()
+        shutdownfn()
